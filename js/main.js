aff68dd3af43c8b73d01999348ab89178bd076ca
Trevor Robinson <tprobinson93@gmail.com>
2016-02-29 16:54:14 -0800

    More changes, getting there

--- js/main.js
+++ js/main.js
@@ -26,11 +26,10 @@
 
 
 
-
 	//Initialize Angular app
 	var app = angular.module("vip", [ ]);
 
-	var autoplay = 1;
+
 
 	app.controller("vipController", function($scope,$http) {
 		/////
@@ -40,20 +39,24 @@
 
 		this.songs = '';
 		this.curSong = '';
+		this.autoplay = 1;
 
 		this.player = document.getElementsByTagName("audio")[0];
+		this.playlist = document.getElementById("playlist");
 
-		this.player.addEventListener('ended', function() {
+		this.player.onended = function() {
 			//Playing finished, shuffle if we autoplay.
-			if( autoplay )
+			if( this.autoplay )
 			{ this.shuffleSong(); }
-		});
+		}.bind(this);
 
 		this.playSong = function(song) {
 
 			//Stop and unregister the old song.
 			this.player.pause();
 			this.player.src = '';
+			if( this.curSong !== null && this.curSong !== '' )
+			{ removeClass(this.playlist.children[this.curSong.index],'active-song'); }
 			this.curSong = '';
 
 			//log
@@ -61,24 +64,52 @@
 
 			//Start the new song.
 			this.curSong = song;
+			addClass(this.playlist.children[this.curSong.index],'active-song');
 			this.player.src = song.location;
 			this.player.play();
+
+			//Trigger the playlist to scroll.
+			this.scrollToSong(song);
+
+
 		};
 
 		this.shuffleSong = function() {
+			//Start a random song.
 			this.playSong(this.songs[Math.floor(Math.random() * this.songs.length)]);
 		};
 
 		this.isCurrentSong = function(song) {
-			return song === this.curSong;
+			return song.index === this.curSong.index;
 		};
 
 		this.init = function() {
-			console.log(this.songs);
+			//Give the song list an index for each song.
+			this.songs.forEach(function(curValue,index,array) { curValue.index = index; });
+
+			//Assign it to the GUI list.
 			$scope.songs = this.songs;
-			if( autoplay )
-			{ this.shuffleSong(); }
+
+			//Make the scrollbar init happen soon
+			window.setTimeout(function(){
+				if( this.autoplay )
+				{ this.shuffleSong(); }
+			}.bind(this),1000); // scrollbar initialization
+
+
 		};
+
+		this.scrollToSong = function(song) {
+
+			//Get the elements' height, since this could change.
+			var height = this.playlist.firstElementChild.offsetHeight;
+
+			console.log(this.playlist.scrollTop + ' by interval '+ height +' to '+height*this.curSong.index);
+
+			//Make the playlist scroll to the currently playing song.
+			this.playlist.scrollTo(0,height*this.curSong.index);
+
+		}.bind(this);
 
 
 		/////
@@ -102,8 +133,55 @@
 		// xhr.send();
 		$http.get('roster.xml')
 			.then(function(res) {
-				$scope.songs = x2js.dom2js(this.responseXML).playlist.trackList.track;
+				$scope.vipCtrl.songs = x2js.xml2js(res.data).playlist.trackList.track;
+				$scope.vipCtrl.init();
 			});
 	});
 
 })();
+
+
+// Animation functions
+
+// Adapted from http://stackoverflow.com/questions/21474678/scrolltop-animation-without-jquery
+function scrollToSmooth(scrollLocation,scrollDuration) {
+    // const   scrollHeight = window.scrollY,
+	var		scrollHeight = window.scrollY,
+            scrollStep = Math.PI / ( scrollDuration / 15 ),
+            cosParameter = scrollHeight / 2;
+    var     scrollCount = 0,
+            scrollMargin;
+    requestAnimationFrame(step);
+    function step () {
+        setTimeout(function() {
+            if ( window.scrollY != scrollLocation ) {
+                    requestAnimationFrame(step);
+                scrollCount = scrollCount + 1;
+                scrollMargin = cosParameter - cosParameter * Math.cos( scrollCount * scrollStep );
+                window.scrollTo( 0, ( scrollHeight - scrollMargin ) );
+            }
+        }, 15 );
+    }
+}
+
+function hasClass(el, className) {
+  if (el.classList)
+    return el.classList.contains(className);
+  else
+    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+}
+
+function addClass(el, className) {
+  if (el.classList)
+    el.classList.add(className);
+  else if (!hasClass(el, className)) el.className += " " + className;
+}
+
+function removeClass(el, className) {
+  if (el.classList)
+    el.classList.remove(className);
+  else if (hasClass(el, className)) {
+    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+    el.className=el.className.replace(reg, ' ');
+  }
+}
