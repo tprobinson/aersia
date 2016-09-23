3c756319dd7f73ae9bbb9caf9fd6e1b4d19bf53e
Trevor Robinson <tprobinson93@gmail.com>
2016-09-23 12:42:21 -0700

    Moved files around, wrote build processes to more clearly support dev and production, JS is now uglified in production, broke out a lot of main.js functions into different files, all images are optimized now

---
+++ js/polyfills/svgicons.js
@@ -0,0 +1,13 @@
+//Test for SVG support and polyfill if no. https://css-tricks.com/svg-sprites-use-better-icon-fonts/
+/MSIE|Trident/.test(navigator.userAgent) && document.addEventListener('DOMContentLoaded', function () {
+  [].forEach.call(document.querySelectorAll('svg'), function (svg) {
+	var use = svg.querySelector('use');
+
+	if (use) {
+	  var object = document.createElement('object');
+	  object.data = use.getAttribute('xlink:href');
+	  object.className = svg.getAttribute('class');
+	  svg.parentNode.replaceChild(object, svg);
+	}
+  });
+});
